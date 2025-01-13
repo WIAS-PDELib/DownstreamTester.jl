bdcb5bbce58f200c0852d3e043b46cf97ed6a562
Patrick Jaap <patrick.jaap@wias-berlin.de>
2025-01-13 14:55:28 +0100

    Cleanup, formatting and code quality checks
    - ExplicitImports
    - add missing docstring for module (add something meaningful)
    - Aqua Checks
    - fix yesterday test
    - deactivate FailureInfo check
    - reorganize Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,23 @@
 using DownstreamTester
 using Test
+using Aqua
+using ExplicitImports
 
 println("Testing...")
 
-include("test_yesterday.jl")
-include("test_infos.jl")
+include("test_previous_day.jl")
+#include("test_infos.jl")
+
+@testset "ExplicitImports" begin
+    @test ExplicitImports.check_no_implicit_imports(DownstreamTester) === nothing
+    @test ExplicitImports.check_no_stale_explicit_imports(DownstreamTester) === nothing
+end
+
+@testset "Aqua" begin
+    Aqua.test_all(DownstreamTester)
+
+end
+
+@testset "UndocumentedNames" begin
+    @test isempty(Docs.undocumented_names(DownstreamTester))
+end
--- test/test_yesterday.jl
+++
@@ -1,8 +0,0 @@
-using Dates
-
-import DownstreamTester.yesterday
-@testset "yesterday()" begin
-    @test yesterday(Date(2024, 12, 15)) == Date(2024, 12, 14)
-    @test yesterday(Date(2024, 12, 1)) == Date(2024, 11, 30)
-    @test yesterday(Date(2024, 1, 1)) == Date(2023, 12, 31)
-end

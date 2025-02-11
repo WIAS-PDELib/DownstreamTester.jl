fbdf9c832025b13d4c8c2ad1bdc5e046a411eb19
Jan Philipp Thiele <thiele@wias-berlin.de>
2025-02-11 06:36:36 +0100

    Don't open XML issue every day

--- src/nightly.jl
+++ src/nightly.jl
@@ -60,10 +60,10 @@ If new failing tests are found it will open an issue on the package repository.
 If tests are passing it will report on the related issue and
 close it if all reported tests therein pass.
 """
-function nightly(configfile::String = "DownstreamTester.json"; do_clone=true)
+function nightly(configfile::String = "DownstreamTester.json"; do_clone = true)
     config = JSON.parsefile(configfile)
     nightlyconfig = config["repo"]
-    process_git!(nightlyconfig,do_clone)
+    process_git!(nightlyconfig, do_clone)
     latest = string(nightlyconfig["githashes"][1])
     ver = string(VERSION)
     name = nightlyconfig["name"]
@@ -89,8 +89,8 @@ function nightly(configfile::String = "DownstreamTester.json"; do_clone=true)
         end
         xmlfilename = name * "_nightly_" * latest * "_v" * ver * ".xml"
         nightly_testrun(name, nightlyconfig["path"], logpath, xmlfilename)
-        if !isfile(logpath*xmlfilename)
-            @info "XML file not found, tests did not run correctly. Opening issue."
+        if !isfile(logpath * xmlfilename)
+            @info "XML file not found, tests did not run correctly."
 
             new = Set{FailureInfo}()
             failure = FailureInfo(
@@ -99,59 +99,65 @@ function nightly(configfile::String = "DownstreamTester.json"; do_clone=true)
                 "XML file not found, check output of GitHub action",
                 ".github/workflows/downstreamtester.yml"
             )
-            push!(new,failure)
+            if new in prev.failures
+                @info "Problem already known."
+            else
+                @info "Opening issue."
+                push!(new, failure)
 
-            title = "DownstreamTester nightly not running " * latest[1:6]
-            preamble = "Dear all,\n\n"
-            preamble *= "this is DownstreamTester.jl reporting a failing *nightly* test run.\n\n"
-            preamble *= "No XML output found, please check output of GitHub action for more details."
+                title = "DownstreamTester nightly not running " * latest[1:6]
+                preamble = "Dear all,\n\n"
+                preamble *= "this is DownstreamTester.jl reporting a failing *nightly* test run.\n\n"
+                preamble *= "No XML output found, please check output of GitHub action for more details."
 
-            issueinfo = open_issue(nightlyconfig["reporting"],title,preamble,["nightly"],new)
-            push!(issues,issueinfo)
-            issuefilename = name * "_nightly_issues.json"
-            issuefile = open(logpath * issuefilename,"w")
-            JSON.print(issuefile,issues,2)
-            close(issuefile)
-            git_add_file(issuefilename,logpath)
-        end
-
-        info = process_nightlylog(logpath * xmlfilename, latest, name)
-        diff = diff_failures(prev.failures, info.failures)
-
-        if !isempty(diff.new)
-            @info "New failures since last run, opening issue."
-
-            title = "DownstreamTester nightly failure " * latest[1:6]
-            preamble = "Dear all,\n\n"
-            preamble *= "this is DownstreamTester.jl reporting a new *nightly* regression \n\n"
-            preamble *= "* new revision: " * revision_link(url, latest) * " with Julia v" * ver * "\n"
-            if prev.commithash != "" && prev.nightlyversion != ""
-                preamble *= "* old revision: " * revision_link(url, prev.commithash) * " with Julia v" * prev.nightlyversion * "\n"
-                if prev.commithash != latest
-                    preamble *= "* compare revisions: [`diff`](" * url * "/compare/" * prev.commithash * "..." * latest * ")\n"
-                end
+                issueinfo = open_issue(nightlyconfig["reporting"], title, preamble, ["nightly"], new)
+                push!(issues, issueinfo)
+                issuefilename = name * "_nightly_issues.json"
+                issuefile = open(logpath * issuefilename, "w")
+                JSON.print(issuefile, issues, 2)
+                close(issuefile)
+                git_add_file(issuefilename, logpath)
             end
-            issueinfo = open_issue(nightlyconfig["reporting"], title, preamble, ["nightly"], diff.new)
-            push!(issues, issueinfo)
+        else
+
+            info = process_nightlylog(logpath * xmlfilename, latest, name)
+            diff = diff_failures(prev.failures, info.failures)
+
+            if !isempty(diff.new)
+                @info "New failures since last run, opening issue."
+
+                title = "DownstreamTester nightly failure " * latest[1:6]
+                preamble = "Dear all,\n\n"
+                preamble *= "this is DownstreamTester.jl reporting a new *nightly* regression \n\n"
+                preamble *= "* new revision: " * revision_link(url, latest) * " with Julia v" * ver * "\n"
+                if prev.commithash != "" && prev.nightlyversion != ""
+                    preamble *= "* old revision: " * revision_link(url, prev.commithash) * " with Julia v" * prev.nightlyversion * "\n"
+                    if prev.commithash != latest
+                        preamble *= "* compare revisions: [`diff`](" * url * "/compare/" * prev.commithash * "..." * latest * ")\n"
+                    end
+                end
+                issueinfo = open_issue(nightlyconfig["reporting"], title, preamble, ["nightly"], diff.new)
+                push!(issues, issueinfo)
+            end
+
+            if !isempty(diff.fixed)
+                @info "Fixed failures since last run, " *
+                    "check can  if issue can be closed."
+
+                #Nightly specific start of comment text
+                preamble = "The following tests are passing again "
+                preamble *= "in revision " * revision_link(url, latest)
+                preamble *= " with Julia v" * string(VERSION) * " :tada:\n\n"
+                mark_as_fixed!(issues, diff.fixed, preamble)
+            end
+
+            #Overwrite issue file
+            issuefilename = name * "_nightly_issues.json"
+            issuefile = open(logpath * issuefilename, "w")
+            JSON.print(issuefile, issues, 2)
+            close(issuefile)
+            git_add_file(issuefilename, logpath)
         end
-
-        if !isempty(diff.fixed)
-            @info "Fixed failures since last run, " *
-                "check can  if issue can be closed."
-
-            #Nightly specific start of comment text
-            preamble = "The following tests are passing again "
-            preamble *= "in revision " * revision_link(url, latest)
-            preamble *= " with Julia v" * string(VERSION) * " :tada:\n\n"
-            mark_as_fixed!(issues, diff.fixed, preamble)
-        end
-
-        #Overwrite issue file
-        issuefilename = name * "_nightly_issues.json"
-        issuefile = open(logpath * issuefilename, "w")
-        JSON.print(issuefile, issues, 2)
-        close(issuefile)
-        git_add_file(issuefilename, logpath)
     end
 
     ## Print results to todays file
